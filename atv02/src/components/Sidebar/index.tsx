bdbe700929c1c098ce69e8360d654a24105d5e95
Karen Gonçalves <105525498+karengoncalves8@users.noreply.github.com>
2024-10-31 11:02:44 -0300

    feat: terminando sidebar e inciando pagina de produtos

--- atv02/src/components/Sidebar/index.tsx
+++ atv02/src/components/Sidebar/index.tsx
@@ -4,58 +4,74 @@ import './style.css';
 
 import logo from '../../assets/logo/logo.svg'
 
-import { BiSolidChevronRightSquare } from "react-icons/bi";
+import { BiSolidChevronLeftSquare } from "react-icons/bi";
 import { AiFillHome, AiFillProduct } from "react-icons/ai";
 import { FaHandHolding, FaPaw, FaUserCircle } from "react-icons/fa";
 import { HiUsers } from "react-icons/hi2";
 import { BsPersonVcardFill } from "react-icons/bs";
 import { FiLogOut } from "react-icons/fi";
+import { useLocation } from 'react-router-dom';
 
 const Sidebar: React.FC = () => {
+    const location = useLocation()
+    
+    const [ collapsed, setCollapsed ] = useState(false)
+
+    const toggleNavbar = () => {
+      const sidebar = document.querySelector('.sidebar')
+
+      if(collapsed){
+        sidebar?.classList.remove('collapsed')
+        setCollapsed(false)
+      }else{
+        sidebar?.classList.add('collapsed')
+        setCollapsed(true)
+      }
+    }
 
     return (
       <aside className='sidebar'>
         <header className='sidebar-header'>
           <img src={logo} className='header-logo'></img>
-          <BiSolidChevronRightSquare className="toggler-icon icon"/>
+          <BiSolidChevronLeftSquare className="toggler-icon icon" onClick={toggleNavbar}/>
         </header>
          
         <nav className="sidebar-nav">
           <ul className="nav-list routes-nav">
-            <li className="nav-item">
+            <li className={location.pathname === '/Inicio' ? 'nav-item active' : 'nav-item'}>
               <div className="nav-link">
                 <AiFillHome className='icon'/>
-                <span>Ínicio</span>
+                <span className="nav-label">Ínicio</span>
               </div>
             </li>
-            <li className="nav-item">
+            <li className={location.pathname === '/Produtos' ? 'nav-item active' : 'nav-item'}>
               <div className="nav-link">
                 <AiFillProduct className='icon' />
-                <span>Produtos</span>
+                <span className="nav-label">Produtos</span>
               </div>
             </li>
-            <li className="nav-item">
+            <li className={location.pathname === '/Servicos' ? 'nav-item active' : 'nav-item'}>
               <div className="nav-link">
                 <FaHandHolding className='icon'/>
-                <span>Serviços</span>
+                <span className="nav-label">Serviços</span>
               </div>
             </li>
-            <li className="nav-item">
+            <li className={location.pathname === '/Clientes' ? 'nav-item active' : 'nav-item'}>
               <div className="nav-link">
                 <HiUsers className='icon' />
-                <span>Clientes</span>
+                <span className="nav-label">Clientes</span>
               </div>
             </li>
-            <li className="nav-item">
+            <li className={location.pathname === '/Pets' ? 'nav-item active' : 'nav-item'}>
               <div className="nav-link">
                 <FaPaw className='icon' />
-                <span>Pets</span>
+                <span className="nav-label">Pets</span>
               </div>
             </li>
-            <li className="nav-item">
+            <li className={location.pathname === '/Funcionarios' ? 'nav-item active' : 'nav-item'}>
               <div className="nav-link">
-                <BsPersonVcardFill className='icon icon-func' />
-                <span>Funcionários</span>
+                <BsPersonVcardFill className='icon' />
+                <span className="nav-label">Funcionários</span>
               </div>
             </li>
           </ul>
@@ -64,13 +80,13 @@ const Sidebar: React.FC = () => {
             <li className="nav-item">
               <div className="nav-link">
                 <FaUserCircle className='icon' />
-                <span>Perfil</span>
+                <span className="nav-label">Perfil</span>
               </div>
             </li>
-            <li className="nav-item">
+            <li className="nav-item logout">
               <div className="nav-link">
                 <FiLogOut className='icon' />
-                <span>Sair</span>
+                <span className="nav-label">Sair</span>
               </div>
             </li>
           </ul>
--- atv02/src/pages/Produtos/index.tsx
+++ atv02/src/pages/Produtos/index.tsx
@@ -3,12 +3,69 @@ import { useNavigate } from 'react-router-dom';
 
 import './style.css';
 
+import { CardPaper } from '../../components/customMUI';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { ptBR } from '@mui/x-data-grid/locales';
+
+const columns: GridColDef<(typeof rows)[number]>[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'nome',
+      headerName: 'Nome',
+      width: 250,
+      editable: true,
+    },
+    {
+      field: 'descricao',
+      headerName: 'Descrição',
+      width: 350,
+      editable: true,
+    },
+    {
+      field: 'preco',
+      headerName: 'Preço',
+      type: 'number',
+      width: 110,
+      editable: true,
+    }
+];
+  
+const rows = [
+    { id: 1, nome: 'Ração Golden', descricao: 'Ração para filhotes da marca Golden', preco: 24.55 },
+    { id: 2, nome: 'Ração Golden', descricao: 'Ração para filhotes da marca Golden', preco: 24.55 },
+    { id: 3, nome: 'Ração Golden', descricao: 'Ração para filhotes da marca Golden', preco: 24.55 },
+    { id: 4, nome: 'Ração Golden', descricao: 'Ração para filhotes da marca Golden', preco: 24.55 },
+    { id: 5, nome: 'Ração Golden', descricao: 'Ração para filhotes da marca Golden', preco: 24.55 },
+    { id: 6, nome: 'Ração Golden', descricao: 'Ração para filhotes da marca Golden', preco: 24.55 },
+    { id: 7, nome: 'Ração Golden', descricao: 'Ração para filhotes da marca Golden', preco: 24.55 }
+
+];
+
+
+  
 function Produtos() {
 
     return (
         <main>
-            
             <h1>Produtos</h1>
+
+            <CardPaper>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                    pagination: {
+                        paginationModel: {
+                        pageSize: 5,
+                        },
+                    },
+                    }}
+                    localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
+                    pageSizeOptions={[5]}
+                    checkboxSelection
+                    disableRowSelectionOnClick
+                />
+            </CardPaper>
         </main>
     )
 }
